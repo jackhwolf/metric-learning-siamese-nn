e9e34a8e916a1399d6445ca2b518c0fb849b3c4d
jackhwolf <jwolf22@wisc.edu>
2021-04-09 18:25:35 -0500

    work

--- src/noise.py
+++ src/noise.py
@@ -3,8 +3,8 @@ import numpy as np
 # mu is P(label) == 1
 def Noise(distance):
     true = np.sign(distance)
-    noisy = true
-    mu = 1 / (1 + np.exp(-1*distance))
+    noisy = 1
+    mu = 1 / (1 + np.exp(-1*true))
     if np.random.rand() > mu:
-        noisy *= -1
+        noisy = -1
     return mu, true, noisy
