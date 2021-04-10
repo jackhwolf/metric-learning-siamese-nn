7f70bef8ba7ad32d8d4b5a53d998c4b898defc0f
jackhwolf <jwolf22@wisc.edu>
2021-04-09 19:16:19 -0500

    work

--- src/experiments/excess_risk.py
+++ src/experiments/excess_risk.py
@@ -51,7 +51,7 @@ class ExcessRiskExperiment(ExperimentBase):
 	def relative_excess_risk(self):
 		self.risk_hat = self.calc_risk_hat()
 		rs, rh = self.risk_star[0], self.risk_hat[0]
-		return (rs-rh) / rs
+		return (rh-rs) / rs
 
 	def calc_risk_hat(self):
 		rh = 0
